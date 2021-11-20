16b93feef2314f9645516a69cace2eab8f35b1b7
Andre Madarang <andre_madarang@hotmail.com>
2021-11-20 17:42:11 -0500

    Initial Lighthouse GraphQL setup for blog

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => ucfirst($this->faker->words(4, true)),
+            'body' => ucfirst($this->faker->sentences(4, true)),
+            'created_at' => $this->faker->dateTimeBetween('-30 days', now()),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Post::factory(50)->create();
     }
 }
